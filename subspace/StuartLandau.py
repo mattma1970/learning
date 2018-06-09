255ccd5c28517043a22e24d9b802c0da1cdcb3ad
mattma1970 <mattma1970@gmail.com>
2018-06-09 16:55:27 +1000

    Bug fix to Stuart Landau equation example

--- subspace/StuartLandau.py
+++ subspace/StuartLandau.py
@@ -5,41 +5,59 @@ import numpy.random as rnd
 
 
 def generate_stuart_landau(intDataCount, dictParas, liInit):
-    # Generates data from stuart landau equations
-    # intDataCount : number of data points to generate
-    # dictPara : dictionary of parameters,namely, mu,gamma,beta,dt and sigma_p
-    # liInit: list of initial values [r, theta]
+    """
+    Generates data from stuart landau equations with optional process noise
+    Note that observation noise is implemented in gerneate_observables_matrix
+    inputs
+    ======
+        intDataCount : int : number of data points to generate
+        dictPara : dictionary of parameters,namely, mu,gamma,beta,dt and sigma_p
+        liInit: list : initial values [r, theta]
+    returns
+    =======
+        npArray : 2 x intDataCount : simulated data from stuart landau equation
+        
+    """
 
     data=np.zeros((2,intDataCount)) # instantiate data array
     data[:,0]=np.array(liInit)  # 2x1 matrix of initial conditions
 
     for counter in np.arange(1,intDataCount):
         r_t = data[0,counter-1];
-        theta_t=data[0,counter-1];
+        theta_t=data[1,counter-1];
 
         # discretized stuart-landau solution
         temp = np.array([r_t+(dictParas['mu']*r_t-r_t**3)*dictParas['dt'],
-                            theta_t + (dictParas['gamma']-dictParas['beta']*r_t**2)*dictParas['dt']])
-        temp2 = temp + np.diag([dictParas['dt'],dictParas['dt']/r_t])@(rnd.multivariate_normal([0,0],[[1,0],[0,1]])*dictParas['sigma_p'])
+                            theta_t + (dictParas['gamma']-dictParas['beta']*r_t**2)*dictParas['dt']]).reshape(2,1)
+        temp2 = temp + np.diag([dictParas['dt'],dictParas['dt']/r_t])@(rnd.randn(2,1)*dictParas['sigma_p'])
 
-        data[:,counter] = np.array(temp2)
+        data[:,counter] = np.array(temp2.reshape(2,))
 
     return data
 
 
 # Generate the extended data matrix using
-def generate_observables_matrix(data,n,obs_noise_std=0.05):
-    # data: full data matrix
-    # n: 1/2 the number of observation functions to use (2n+1)
-    # bAddNoise: boolean indicating if additive noise should be introduced.
+def generate_observables_matrix(data,n,dictParas):
+    """
+    Generate observable matrix using complex exponentials
+    Inputs
+    ======
+        data: np.array : full data matrix
+        n: int: 1/2 the number of observation functions to use (2n+1)
+        dictParas: dictionary containing observation noise value
+    returns
+    =======
+        abs: nparray of 2n+1 x intDataCount : observation noise corrupted observables matrix
+    
+    """
 
     obs = np.zeros((2*n+1,data.shape[1]),dtype=complex)
 
     for column_num in range(data.shape[1]):
-        o = np.array([complex(0, m*data[1,column_num]) for m in np.arange(-n, n+1, 1)])
-        o2= np.array([rnd.normal(0,1) for i in range(2*n+1)])*obs_noise_std
+        o = np.array([complex(np.cos(m*data[1,column_num]),np.sin(m*data[1,column_num])) for m in np.arange(-n, n+1, 1)]).reshape(2*n+1,1)
+        o2= rnd.randn(2*n+1,1)*dictParas['sigma_o']
 
-        obs[:, column_num]=o+o2
+        obs[:, column_num:(column_num+1)]=(o+o2)
 
     return obs
 
--- subspace/myUtils.py
+++ subspace/myUtils.py
@@ -119,14 +119,27 @@ def plot_eigs_cart(lam_true, lam_ord, lam_sub):
     return
 
 
-def plot_point_array(data,marker='+',marker_color='green'):
-    # get data in polar coordinates for plotting
+def plot_point_array(data,marker='+',marker_color='green',blSetXScale=False, x_range_scale=1, title='chart'):
+    """
+    Plot list of complex values on cartesian axes.
+    """
     if type(data) is list:
         data=np.array(data,dtype=complex)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
-    ax.autoscale()
+    ax.set_title(title)
+    ax.title
+    
+    if blSetXScale==True:
+        ax.set_autoscaley_on(True)
+        ax.set_autoscalex_on(False)
+        ax.set_xlim(-1*x_range_scale,1*x_range_scale)
+    else:
+        ax.set_autoscaley_on(True)
+        ax.set_autoscalex_on(True)
+        ax.autoscale()
+    
     #ax.set_xticks(np.arange(-1.1, 1.1, 0.1))
     #ax.set_yticks(np.arange(-1.1, 1.1, 0.1))
     ax.set_xlabel("Real")
